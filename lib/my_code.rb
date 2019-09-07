7a94245449b55ad6b019dcf9c3491162d572a5f8
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-07 18:54:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  return new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  i = 0
+  while i < source_array.length do
+    yield(source_array[i],source_array[i+1])
+    i += 1
+  end
+  return a_value
+end
