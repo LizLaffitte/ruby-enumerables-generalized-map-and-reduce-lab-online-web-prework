3ed7c3c18282588228c399165a7568adcaea03fb
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-07 19:06:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(source_array, starting_point = nil)
     i = 1
   end
   while i < source_array.length do
-    total = yield(total,source_array[i+1])
+    total = yield(total,source_array[i])
     i += 1
   end
   return total
